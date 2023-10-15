bca1ebafca8e58954b0ff2c5bfb0d9508f7f1516
TrevorClute <121002446+TrevorClute@users.noreply.github.com>
2023-10-15 12:26:54 -0400

    Initial Commit

---
+++ MacroTracker/MacroTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacroTrackerApp.swift
+//  MacroTracker
+//
+//  Created by Trevor Clute on 10/15/23.
+//
+
+import SwiftUI
+
+@main
+struct MacroTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
